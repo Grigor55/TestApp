a7a3f7d460809349472d2b2facbee9f52a1c4c86
Grigor <52450179+Grigor55@users.noreply.github.com>
2021-11-11 16:37:20 +0400

    Fix initial changes

---
+++ TestApp 16.35.39/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Grigor Keshishyan on 11.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
+var isTesttt: Bool = true
